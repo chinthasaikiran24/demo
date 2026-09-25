6ce89ccfaf545306a3bf9afa54ac0493bdc416a3
chint <chint@DESKTOP-G41KNM4.lan>
2026-09-25 21:38:32 +0530

    Phase5 Exception code

--- src/main/java/com/example/demo/Phase5Excep.java
+++ src/main/java/com/example/demo/Phase5Excep.java
@@ -1,39 +1,30 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.support.MethodArgumentNotValidException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-
+import DTO.EmployeeRequest;
 import DTO.EmployeeRequestClass;
 import jakarta.validation.Valid;
 
 @RestController
-@RequestMapping("/employeeEntity")
-public class Phase5Entity {
+@RequestMapping("/employee")
+public class Phase5Excep {
 
     private final ObjectMapper objectMapper;
 
-    public Phase5Entity(ObjectMapper objectMapper) {
+    public Phase5Excep(ObjectMapper objectMapper) {
         this.objectMapper = objectMapper;
     }
 
@@ -47,29 +38,27 @@ public class Phase5Entity {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<?> handleValidationErrors(
             MethodArgumentNotValidException ex) {
-
-    	
-    	BindingResult result = ex.getBindingResult();
     	
     	ObjectNode response = objectMapper.createObjectNode();
-    	response.put("Status", "Failed");
-    	
-    	ArrayNode error = objectMapper.createArrayNode();
-    	
-    	for(FieldError  errors : result.getFieldErrors()) {
-    		
-    		String errorField = errors.getField();
-    		String errorMessage = errors.getDefaultMessage();
-    		
-    		 ObjectNode errorObject = objectMapper.createObjectNode();
-    		
-    		 errorObject.put("Field", errorField);
-    		 errorObject.put("Message", errorMessage);
-    		 
-    		 error.add(errorObject);
-    		
-    	}
-    	response.set("errors", error);
+    	response.put("status", "FAILED");
+    	 ArrayNode errors = objectMapper.createArrayNode();
+
+        BindingResult result = ex.getBindingResult();
+        
+        for(FieldError error : result.getFieldErrors() ) {
+        	
+        	String filedName = error.getField();
+        	String fieldValue = error.getDefaultMessage();
+        	
+        	ObjectNode obj = objectMapper.createObjectNode();
+        	
+        	obj.put("Field:- ", filedName);
+        	obj.put("Error ", fieldValue);
+        	
+        	errors.add(obj);
+        	      	
+        }
+        response.put("Errors ", errors);
 
         return ResponseEntity.badRequest().body(response);
     }
